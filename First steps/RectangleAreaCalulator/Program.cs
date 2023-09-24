90407debb8a7a984d4d196e2c88555e41df6c1c6
Carlo Caldura <79699017+CarloCaldura@users.noreply.github.com>
2023-09-24 12:16:54 +0200

    Upload personal developments directly in master

---
+++ First steps/RectangleAreaCalulator/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace RectangleAreaCalulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Rectangle Area Calculator!");
+
+            Console.Write("Enter the lenght of the rectangle: ");
+            double lenght = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the width of the rectangle: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            double area = lenght * width;
+
+            Console.WriteLine($"The area of the rectangle is {area}");
+
+            Console.ReadLine();
+        }
+    }
+}
