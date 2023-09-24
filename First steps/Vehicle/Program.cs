f4e155c558c159927e60535ad0ff258ee9f07781
Carlo Caldura <carlo.caldura@gmail.com>
2023-09-24 20:43:46 +0200

    Implementazione metodo Consumtion

--- First steps/Vehicle/Program.cs
+++ First steps/Vehicle/Program.cs
@@ -1,10 +1,11 @@
-﻿using System.Xml.Schema;
+﻿using System.Numerics;
+using System.Xml.Schema;
 using Microsoft.VisualBasic;
 using Vehicle;
 
 namespace Vehicle
 
-{
+{   // Main Class
     class Vehicle
     {
         // Fields (private vars)
@@ -12,14 +13,20 @@ namespace Vehicle
         private string model;
         private int year;
         private int numberofwheels;
+        private decimal fuelquantity;
+        private decimal distance;
+
 
         // Constructor
-        public Vehicle(string make, string model, int year, int numberofwheels)
+        public Vehicle(string make, string model, int year, int numberofwheels, decimal fuelquantity, decimal distance)
         {
             this.make = make;
             this.model = model;
             this.year = year;
             this.numberofwheels = numberofwheels;
+            this.fuelquantity = fuelquantity;
+            this.distance = distance;
+
         }
 
         // Properties (getters and setters)
@@ -35,6 +42,12 @@ namespace Vehicle
         public int Numberofwheels
         { get { return numberofwheels; } set { numberofwheels = value; } }
 
+        public string Fuelquantity
+        { get { return Fuelquantity; } set { Fuelquantity = value; } }
+
+        public int Distance
+        { get { return Distance; } set { Distance = value; } }
+
 
         // Method to display vehicle information
         public virtual void DisplayInfo()
@@ -43,6 +56,18 @@ namespace Vehicle
             Console.WriteLine($"Model: {model}");
             Console.WriteLine($"Year: {year}");
             Console.WriteLine($"Number of wheels: {numberofwheels}");
+            Console.WriteLine($"Liters of fuel consumed: {fuelquantity}");
+            Console.WriteLine($"Distance travelled: {distance}");
+        }
+
+        // Method to calculate gas consumption
+        public virtual decimal Consumption( decimal fuel, decimal distance) 
+        {
+            if (fuelquantity > 0 & distance > 0) 
+            {
+                return distance / fuelquantity;
+            }
+            return 0;
         }
     }
 
@@ -53,8 +78,8 @@ namespace Vehicle
         public int NumDoors { get; set; }
 
         // Constructor
-        public Car(string make, string model, int year, int numberofwheels, int numDoors)
-            : base(make, model, year, numberofwheels)
+        public Car(string make, string model, int year, int numberofwheels, decimal fuelquantity, decimal distance,int numDoors)
+            : base(make, model, year, numberofwheels, fuelquantity, distance)
         {
             NumDoors = numDoors;
         }
@@ -74,8 +99,8 @@ namespace Vehicle
         public string Type { get; set; }
 
         // Constructor
-        public Motorcycle(string make, string model, int year, int numberofwheels, string type)
-            : base(make, model, year, numberofwheels)
+        public Motorcycle(string make, string model, int year, int numberofwheels, decimal fuelquantity, decimal distance, string type)
+            : base(make, model, year, numberofwheels, fuelquantity, distance)
 
         {
             Type = type;
@@ -93,15 +118,18 @@ namespace Vehicle
     {
         static void Main()
         {
-            Car myCar = new("Citroen", "Picasso", 2002, 4, 5);
-            Motorcycle myMotorcycle = new("Suzuki", "Hayabusa", 2018, 2, "Sportbike");
+            Car myCar = new("Citroen", "Picasso", 2002, 4, 10, 225, 5);
+            Motorcycle myMotorcycle = new("Suzuki", "Hayabusa", 2018, 2, 5, 80, "Sportbike");
 
             Console.WriteLine("Car Information:");
             myCar.DisplayInfo();
+            decimal carConsumption = myCar.Consumption(10, 225);
+            Console.WriteLine($"The car {myCar.make} {myCar.Model} has a consumption rate of {myCar.Consumption} km/L");
 
             Console.WriteLine("\nMotorcycle Information:");
             myMotorcycle.DisplayInfo();
-
+            decimal motorcicleConsumption = myMotorcycle.Consumption(5, 80);
+            Console.WriteLine($"The motorcicle {myMotorcycle.make} {myMotorcycle.Model} has a consumption rate of {myMotorcycle.Consumption} km/L");
         }
     }
 }
